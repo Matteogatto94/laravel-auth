e85044b531e03732f446db8e4e0e9659a3737834
Matteogatto94 <cosenza94@live.it>
2023-01-16 14:46:00 +0100

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/', function () {
 });
 
 
+
+
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameters([
